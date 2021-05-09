85be7d80d9ffb0091b72f054e37bb3dfbde68607
Poseidoncode <66759971+Poseidoncode@users.noreply.github.com>
2021-05-09 23:11:58 +0800

    config

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -18,9 +18,7 @@ export default {
     };
   },
   mounted() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((response) => (this.info = response));
+    axios.get("capi.php").then((response) => (this.info = response));
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/sideproject/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sideproject/'
+    : '/',
+  devServer: {
+      proxy: "https://tw.rter.info/",
+      port: 80,
+      open: process.platform === "darwin",
+    },
+}
