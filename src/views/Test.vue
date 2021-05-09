5a979ef339546e9c814c290f69b90792ab87e98e
Poseidoncode <66759971+Poseidoncode@users.noreply.github.com>
2021-05-09 19:15:10 +0800

    Initial commit

---
+++ src/views/Test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="test">123</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((response) => (this.info = response));
+  },
+};
+</script>
