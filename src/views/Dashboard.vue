61cd590590e359a7a392ec9fe9539082e6cbe2cd
Poseidoncode <66759971+Poseidoncode@users.noreply.github.com>
2021-05-27 23:47:17 +0800

    dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">123</div>
+  <addCount />
+</template>
+
+<script>
+// @ is an alias to /src
+
+import { provide } from "vue";
+import addCount from "@/components/provide/addCount.vue";
+import index from "@/composition/provide/index";
+
+export default {
+  name: "Provide",
+  components: {
+    addCount,
+  },
+  setup() {
+    provide("index", index);
+    console.log("index", index);
+    index.addCount();
+  },
+};
+</script>
